3e1c7cefcec54d4f8769932318348e7908c72272
Risto Vuorinen <risto@medical.fi>
2026-04-14 22:03:39 +0300

    fix: update next config serverExternalPackages to match next 15 spec

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['pdfmake'],
-  },
+  serverExternalPackages: ['pdfmake'],
   images: {
     remotePatterns: [
       {
