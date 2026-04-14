65a813ec80c59e5cc82ec4f929e9b23601dfe2f1
Risto Vuorinen <risto@medical.fi>
2026-04-14 21:12:13 +0300

    feat: add redirect from www.eqilo.fi to eqilo.fi

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.eqilo.fi',
+          },
+        ],
+        destination: 'https://eqilo.fi/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
